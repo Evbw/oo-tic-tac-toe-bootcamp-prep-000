41559bdf636ff3d39692344a67c550ce147b15e3
Everett <everett.bowline@gmail.com>
2018-03-13 02:00:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,16 +50,6 @@ class TicTacToe
     end
   end
   
-  def turn_count
-   move_counter = 0
-   @board.each do |move|
-      if move == "X" || move =="O"
-        move_counter += 1
-     end
-    end
-    move_counter
-  end
-  
   def current_player
      turn_count % 2 == 0 ? "X" : "O"
   end
