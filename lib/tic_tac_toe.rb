4ce5eb1bdaa301366ce3b85b8b0eb1fac27e21a7
Everett <everett.bowline@gmail.com>
2018-03-13 02:01:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,13 +82,13 @@ class TicTacToe
     end
   end
 
-  def play(@board)
-    while !over?(@board)
-      turn(@board)
+  def play
+    while !over?
+      turn
     end
-    if won?(@board)
-      puts "Congratulations #{winner(@board)}!"
-    elsif draw?(@board)
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
         puts "Cat's Game!"
     end
   end
