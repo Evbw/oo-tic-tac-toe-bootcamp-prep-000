547c28abcd7faf71d1216de0e7f51bdf4bca05e6
Everett <everett.bowline@gmail.com>
2018-03-13 02:03:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,8 @@ class TicTacToe
   
   def won?
     WIN_COMBINATIONS.detect do |win|
-      @board[win[0]] == @board[win[1]] && @board[win[1]] == @board[win[2]] && position_taken?(@board, win[0])
+     if @board[win[0]] == @board[win[1]] && @board[win[1]] == @board[win[2]] && position_taken?(@board, win[0])
+       return win
     end
   end
 
